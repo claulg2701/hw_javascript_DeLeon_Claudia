9d14ba113e6dece292e17a7be617e3d6a9172413
Claudia De Leon <claulg2701@gmail.com>
2016-02-05 17:48:59 -0500

    Add 5 objects with the last 5 US presidents

--- js/main.js
+++ js/main.js
@@ -1,1 +1,42 @@
 var iceCream = [ 'strawberry', 'chocolate', 'coconut' ];
+iceCream = 'pecan';
+
+var presidentOne = {
+    firstName: 'Barack',
+    lastName:'Obama',
+    termLength: 2,
+    party: 'Democratic',
+    yearsOfPresidency: 8
+};
+
+var presidentTwo = {
+    firstName: 'George',
+    lastName:'Bush',
+    termLength: 2,
+    party: 'Republican',
+    yearsOfPresidency: 8
+};
+
+var presidentThree = {
+    firstName: 'Bill',
+    lastName:'Clinton',
+    termLength: 2,
+    party: 'Democratic',
+    yearsOfPresidency: 8
+};
+
+var presidentFour = {
+    firstName: 'George',
+    lastName:'Bush',
+    termLength: 1,
+    party: 'Republican',
+    yearsOfPresidency: 4
+};
+
+var presidentFive = {
+    firstName: 'Ronald',
+    lastName:'Reagan',
+    termLength: 2,
+    party: 'Republican',
+    yearsOfPresidency: 8
+};
